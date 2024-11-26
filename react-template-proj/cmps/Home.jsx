d3cba679ce5b21277fd4fa992a8a326d0ef49486
Ohado <ohad578@gmail.com>
2024-11-26 09:28:58 +0200

    first commit

---
+++ react-template-proj/cmps/Home.jsx
@@ -0,0 +1,10 @@
+const { useState, useEffect } = React
+
+export function Home() {
+    return (
+        <section className="home">
+            <h2>Your personal book manager</h2>
+       </section>
+    )
+}
+
